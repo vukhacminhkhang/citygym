18e509d6f48fd450c5bdf50f332a145cab7f87d7
khang <davidvukhang@gmail.com>
2023-10-29 11:09:38 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Javascript lesson1</title>
+</head>
+<style type="text/css" media="screen">
+	body{
+		text-align: center;
+	}
+</style>
+<body>
+	<h3>Click vào button đi ông tướng</h3>
+	<button type="button" onclick="alert('helloworld')">click</button>
+</body>
+</html>
